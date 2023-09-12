fa642127aac451ccd0198f8bee7a3f557dae4317
DeWalle <ashiruhabeeb47@gmail.com>
2023-09-12 10:55:40 +0100

    render database url added and testd ok

--- config/config.go
+++ config/config.go
@@ -5,6 +5,8 @@ import "github.com/spf13/viper"
 type Cfg struct {
 	Db_URL			string	`mapstructure:"DB_URL"`
 	Server_Port		string	`mapstructure:"SERVER_PORT"`
+	Internal_URL	string	`mapstructure:"INTERNAL_URL"`
+	External_URL	string	`mapstructure:"EXTERNAL_URL"`
 }
 
 func LoadEnvVariables(path string)(c Cfg, err error){
