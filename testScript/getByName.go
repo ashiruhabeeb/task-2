4a34ed39120b9595a5e8fa1d3f5f6cafea900e52
DeWalle <ashiruhabeeb47@gmail.com>
2023-09-13 17:12:17 +0100

    Go Native test script for all endpoints added

--- testScript/getByName.go
+++ testScript/getByName.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+  "fmt"
+  "net/http"
+  "io/ioutil"
+)
+
+func GetByName() {
+
+  url := "https://hnginternship-task2.onrender.com/api/?name=Adekunle%20Alao"
+  method := "GET"
+
+  client := &http.Client {
+  }
+  req, err := http.NewRequest(method, url, nil)
+
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  res, err := client.Do(req)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  defer res.Body.Close()
+
+  body, err := ioutil.ReadAll(res.Body)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  fmt.Println(string(body))
+}
